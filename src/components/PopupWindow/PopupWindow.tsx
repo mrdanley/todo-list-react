74cc55636400299765c49ed2b9a8153fbe2b22d1
mrdanley <ledaniel89@gmail.com>
2019-03-06 22:48:38 -0800

    add popup for input error, fixed component rounded edges

---
+++ src/components/PopupWindow/PopupWindow.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { Modal, Button } from "react-bootstrap";
+import "./PopupWindow.css";
+
+interface Props {
+  show: boolean;
+  onClose: () => void;
+  message: string;
+}
+
+const PopupWindow = (props: Props) => {
+  const { show, onClose, message } = props;
+  return (
+    <Modal
+      show={show}
+      onHide={onClose}
+      size="sm"
+      dialogClassName="popup-window"
+    >
+      <Modal.Header>
+        <Modal.Title>{message}</Modal.Title>
+      </Modal.Header>
+
+      <Modal.Footer>
+        <Button onClick={onClose}>Ok</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default PopupWindow;
--- src/components/TodoInput/TodoInput.tsx
+++ src/components/TodoInput/TodoInput.tsx
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
-import { Button, FormGroup, Form, Row, Col } from "react-bootstrap";
+import { Button, FormGroup, Form, Row, Col, ModalBody } from "react-bootstrap";
 import "./TodoInput.css";
 import autobind from "autobind-decorator";
 import { TodoItem } from "../../App";
 import uuid from "uuid";
+import { Modal } from "react-bootstrap";
+import PopupWindow from "../PopupWindow/PopupWindow";
 
 interface Props {
   addTodo: (todo: TodoItem) => void;
 }
 interface State {
   todoInput: string;
+  showInputError: boolean;
 }
 
 @autobind
@@ -17,45 +20,66 @@ class TodoInput extends Component<Props, State> {
   constructor(props: any) {
     super(props);
     this.state = {
-      todoInput: ""
+      todoInput: "",
+      showInputError: false
     };
   }
 
   public render() {
+    const { todoInput, showInputError } = this.state;
     return (
-      <Form onSubmit={this.handleSubmit} className="todo-input-bar">
-        <FormGroup>
-          <Row>
-            <Col sm={8} md={9} lg={10} className="input-col">
-              <Form.Control
-                type="input"
-                placeholder="Ex. Buy groceries"
-                value={this.state.todoInput}
-                onChange={(e: any) =>
-                  this.setState({ todoInput: e.target.value })
-                }
-              />
-            </Col>
-            <Col sm={4} md={3} lg={2} className="button-col">
-              <Button variant="primary" type="submit" block className="button">
-                Add
-              </Button>
-            </Col>
-          </Row>
-        </FormGroup>
-      </Form>
+      <React.Fragment>
+        <Form onSubmit={this.handleSubmit} className="todo-input-bar">
+          <FormGroup>
+            <Row>
+              <Col sm={8} md={9} lg={10} className="input-col">
+                <Form.Control
+                  type="input"
+                  placeholder="Ex. Buy groceries"
+                  value={todoInput}
+                  onChange={(e: any) =>
+                    this.setState({ todoInput: e.target.value })
+                  }
+                  className="todo-form-input"
+                />
+              </Col>
+              <Col sm={4} md={3} lg={2} className="button-col">
+                <Button
+                  variant="primary"
+                  type="submit"
+                  block
+                  className="add-todo-button"
+                >
+                  Add
+                </Button>
+              </Col>
+            </Row>
+          </FormGroup>
+        </Form>
+
+        <PopupWindow
+          message="Empty input!"
+          show={showInputError}
+          onClose={() => this.setState({ showInputError: false })}
+        />
+      </React.Fragment>
     );
   }
 
   private handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    const newTodo: TodoItem = {
-      todoString: this.state.todoInput,
-      checked: false,
-      id: uuid.v4()
-    };
-    this.props.addTodo(newTodo);
-    this.setState({ todoInput: "" });
+
+    if (this.state.todoInput !== "") {
+      const newTodo: TodoItem = {
+        todoString: this.state.todoInput,
+        checked: false,
+        id: uuid.v4()
+      };
+      this.props.addTodo(newTodo);
+      this.setState({ todoInput: "" });
+    } else {
+      this.setState({ showInputError: true });
+    }
   }
 }
 
