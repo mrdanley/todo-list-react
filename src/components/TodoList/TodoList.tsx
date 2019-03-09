e44b2431f25f530361d58d545e85b8f8aef78e2a
mrdanley <ledaniel89@gmail.com>
2019-03-08 22:54:30 -0800

    add check todo on row click

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -5,78 +5,84 @@ import "./TodoList.css";
 import { TodoItem } from "../../App";
 
 interface Props {
-  todos: TodoItem[];
-  setTodos: (todos: TodoItem[]) => void;
+    todos: TodoItem[];
+    setTodos: (todos: TodoItem[]) => void;
 }
 interface State {}
 
 @autobind
 class TodoList extends Component<Props, State> {
-  public render() {
-    const { todos } = this.props;
-    return (
-      <div className="todo-list">
-        {todos.length === 0 ? (
-          <div className="empty-list">No Todos</div>
-        ) : (
-          <React.Fragment>
-            <Button
-              variant="secondary"
-              onClick={() => this.props.setTodos([])}
-              block
-              className="clear-todos-button"
+    public render() {
+        const { todos } = this.props;
+        return (
+            <div className="todo-list">
+                {todos.length === 0 ? (
+                    <div className="empty-list">No Todos</div>
+                ) : (
+                    <React.Fragment>
+                        <Button
+                            variant="secondary"
+                            onClick={() => this.props.setTodos([])}
+                            block
+                            className="clear-todos-button"
+                        >
+                            Clear all items
+                        </Button>
+                        <Table striped bordered hover>
+                            <tbody>{this.createTodoList()}</tbody>
+                        </Table>
+                    </React.Fragment>
+                )}
+            </div>
+        );
+    }
+
+    private createTodoList(): any {
+        const { todos } = this.props;
+        return todos.map(todo => (
+            <tr
+                key={todo.id}
+                onClick={e => {
+                    e.persist();
+                    const type = (e.target as any).type;
+                    this.handleEditTodos(type ? type : "checkbox", todo.id);
+                }}
             >
-              Clear all items
-            </Button>
-            <Table striped bordered hover>
-              <tbody>{this.createTodoList()}</tbody>
-            </Table>
-          </React.Fragment>
-        )}
-      </div>
-    );
-  }
+                <td>
+                    <Form.Check
+                        inline
+                        type="checkbox"
+                        checked={todo.checked}
+                        onChange={() => {}}
+                    />
+                    {todo.todoString}
+                    <Button
+                        variant="danger"
+                        className="delete-button"
+                        type="button"
+                    >
+                        X
+                    </Button>
+                </td>
+            </tr>
+        ));
+    }
 
-  private createTodoList(): any {
-    const { todos } = this.props;
-    return todos.map(todo => (
-      <tr key={todo.id}>
-        <td>
-          <Form.Check
-            inline
-            type="checkbox"
-            checked={todo.checked}
-            onChange={() => this.handleEditTodos("check", todo.id)}
-          />
-          {todo.todoString}
-          <Button
-            variant="danger"
-            className="delete-button"
-            onClick={() => this.handleEditTodos("delete", todo.id)}
-            type="button"
-          >
-            X
-          </Button>
-        </td>
-      </tr>
-    ));
-  }
+    private handleEditTodos(type: string, id: string) {
+        const { todos } = this.props;
+        let newTodos: TodoItem[];
+        newTodos =
+            type === "checkbox"
+                ? todos.map(todo => {
+                      if (todo.id === id) {
+                          return { ...todo, checked: !todo.checked };
+                      }
+                      return todo;
+                  })
+                : todos.filter(todo => todo.id !== id);
 
-  private handleEditTodos(type: string, id: string) {
-    const { todos } = this.props;
-    let newTodos: TodoItem[];
-    newTodos =
-      type === "check"
-        ? todos.map(todo => {
-            if (todo.id === id) {
-              return { ...todo, checked: !todo.checked };
-            }
-            return todo;
-          })
-        : todos.filter(todo => todo.id !== id);
-
-    this.props.setTodos(newTodos);
-  }
+        this.props.setTodos(newTodos);
+    }
 }
 
 export default TodoList;
